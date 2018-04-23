2e736d0be6715924d18c6a1c148d6a86ca494ea9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# ASPxVerticalGrid - How to calculate the total summary based on values of other summaries
+
+
+This sample illustrates how to calculate the total summary value based on values of other summaries.
+
+
+<h3>Description</h3>
+
+&nbsp;
+
+<br/>
+
+
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+    Protected Sub ASPxVerticalGrid1_CustomSummaryCalculate(ByVal sender As Object, ByVal e As DevExpress.Data.CustomSummaryEventArgs)
+        Dim orderedUnits As ASPxVerticalGridSummaryItem = (TryCast(sender, ASPxVerticalGrid)).TotalSummary("UnitsOnOrder")
+        Dim averagePrice As ASPxVerticalGridSummaryItem = (TryCast(sender, ASPxVerticalGrid)).TotalSummary("UnitPrice")
+        Dim intOrderedUnits As Int32 = Convert.ToInt32(DirectCast(sender, ASPxVerticalGrid).GetTotalSummaryValue(orderedUnits))
+        Dim intAveragePrice As Int32 = Convert.ToInt32(DirectCast(sender, ASPxVerticalGrid).GetTotalSummaryValue(averagePrice))
+        e.TotalValue = intOrderedUnits * intAveragePrice
+    End Sub
+End Class
